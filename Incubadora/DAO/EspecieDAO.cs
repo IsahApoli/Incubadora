72f3b5200ef56f956e465d2fcbadfd23f95d3369
Isah Apolinario <isa.apoli26@gmail.com>
2026-05-23 18:32:24 -0300

    add usuario e espcie DAO

---
+++ Incubadora/DAO/EspecieDAO.cs
@@ -0,0 +1,57 @@
+﻿using Incubadora.DAO;
+using Incubadora.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+
+namespace Incubadora.DAO
+{
+    public class EspecieDAO : PadraoDAO<EspecieViewModel>
+    {
+        protected override void SetTabela()
+        {
+            Tabela = "Especies";
+        }
+
+        protected override SqlParameter[] CriaParametros(EspecieViewModel model)
+        {
+            // Tratamento de valor nulo para a imagem [6, 7]
+            object imgByte = model.ImagemEmByte ?? (object)DBNull.Value;
+
+            return new SqlParameter[] {
+            new SqlParameter("id", model.Id),
+            new SqlParameter("nomeEspecie", model.NomeEspecie),
+            new SqlParameter("tempMin", model.TemperaturaMin),
+            new SqlParameter("tempMax", model.TemperaturaMax),
+            new SqlParameter("umidMin", model.UmidadeMin),
+            new SqlParameter("umidMax", model.UmidadeMax),
+            new SqlParameter("luzIdeal", model.LuminosidadeIdeal),
+            new SqlParameter("tempo", model.TempoIncubacao),
+            new SqlParameter("imagem", imgByte)
+        };
+        }
+
+        protected override EspecieViewModel MontaModel(DataRow registro)
+        {
+            EspecieViewModel e = new EspecieViewModel
+            {
+                Id = Convert.ToInt32(registro["id"]),
+                NomeEspecie = registro["nomeEspecie"].ToString(),
+                TemperaturaMin = Convert.ToDecimal(registro["temperaturaMin"]),
+                TemperaturaMax = Convert.ToDecimal(registro["temperaturaMax"]),
+                UmidadeMin = Convert.ToDecimal(registro["umidadeMin"]),
+                UmidadeMax = Convert.ToDecimal(registro["umidadeMax"]),
+                LuminosidadeIdeal = Convert.ToDecimal(registro["luminosidadeIdeal"]),
+                TempoIncubacao = Convert.ToInt32(registro["tempoIncubacao"])
+            };
+
+            // Recupera a imagem do banco se ela não for nula [6]
+            if (registro["imagemAnimal"] != DBNull.Value)
+                e.ImagemEmByte = registro["imagemAnimal"] as byte[];
+
+            return e;
+        }
+    }
+}
---
+++ Incubadora/DAO/UsuariosDAO.cs
@@ -0,0 +1,47 @@
+﻿using Incubadora.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Incubadora.DAO
+{
+    public class UsuarioDAO : PadraoDAO<UsuarioViewModel>
+    {
+        protected override void SetTabela()
+        {
+            Tabela = "Usuarios";
+        }
+
+        protected override SqlParameter[] CriaParametros(UsuarioViewModel model)
+        {
+            object fazendaIdValue = model.FazendaId ?? (object)DBNull.Value;
+
+            SqlParameter[] parametros = new SqlParameter[]
+            {
+                new SqlParameter("id", model.Id),
+                new SqlParameter("nome", model.Nome),
+                new SqlParameter("email", model.Email),
+                new SqlParameter("senha", model.Senha), // Recomenda-se salvar o Hash [6]
+                new SqlParameter("tipoUsuario", model.TipoUsuario),
+                new SqlParameter("fazendaId", fazendaIdValue)
+            };
+
+            return parametros;
+        }
+
+        protected override UsuarioViewModel MontaModel(DataRow registro)
+        {
+            UsuarioViewModel u = new UsuarioViewModel();
+            u.Id = Convert.ToInt32(registro["id"]);
+            u.Nome = registro["nome"].ToString();
+            u.Email = registro["email"].ToString();
+            u.Senha = registro["senha"].ToString();
+            u.TipoUsuario = registro["tipoUsuario"].ToString();
+
+            if (registro["fazendaId"] != DBNull.Value)
+                u.FazendaId = Convert.ToInt32(registro["fazendaId"]);
+
+            return u;
+        }
+    }
+}
